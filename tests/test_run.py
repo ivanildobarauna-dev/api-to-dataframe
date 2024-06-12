fbbf03b81221f28b7a7d338ce50dc2edb3330027
Ivanildo Barauna de Souza Junior <ivanildo.jnr@outlook.com>
2024-06-11 22:25:30 -0300

    feat: Class Methods Define + write tests

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,5 +1,19 @@
 import pytest
-from src.api_to_dataframe import run
+import src.api_to_dataframe as apitodf
 
-def test_main():
-    assert True
+
+def test_constructor_without_param():
+    with pytest.raises(ValueError):
+        new_client = apitodf.ClientBuilder(endpoint="")
+
+
+@pytest.fixture()
+def test_constructor_with_param():
+    expected_result = "param1"
+    new_client = apitodf.ClientBuilder(endpoint=expected_result)
+    assert new_client.endpoint == expected_result
+    return new_client
+
+
+def test_response_to_json_with_valid_endpoint(test_constructor_with_param):
+    pass
