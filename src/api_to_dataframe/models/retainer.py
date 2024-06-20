ed5f48b64e19ed65c58283cc637fb7240009bb90
Ivanildo Barauna de Souza Junior <ivanildo.jnr@outlook.com>
2024-06-20 14:36:25 -0300

    feat: RetryStrategy development

--- src/api_to_dataframe/models/retainer.py
+++ src/api_to_dataframe/models/retainer.py
@@ -1,8 +1,30 @@
 import time
+from enum import Enum
+
+
+class Strategies(Enum):
+    NoRetryStrategy = 0
+    LinearRetryStrategy = 1
+    ExponentialRetryStrategy = 2
 
 
 def RetryStrategies(func):
-    print("Chamada a função RetryStrategies, com os params:", func)
     def wrapper(*args, **kwargs):
-        print("Chamada do wrapper com os argumentos: ", args, kwargs)
+        attemp = 0
+        while attemp < args[0].retries:
+            try:
+                return func(*args, **kwargs)
+            except Exception as e:
+                attemp += 1
+
+                if args[0].retry_strategy == Strategies.NoRetryStrategy:
+                    break
+                elif args[0].retry_strategy == Strategies.LinearRetryStrategy:
+                    time.sleep(args[0].delay)
+                elif args[0].retry_strategy == Strategies.ExponentialRetryStrategy:
+                    time.sleep(args[0].delay * 2 ** attemp)
+
+                if attemp == args[0].retries:
+                    print(f"Failed after {args[0].retries} attempts")
+                    raise e
     return wrapper
